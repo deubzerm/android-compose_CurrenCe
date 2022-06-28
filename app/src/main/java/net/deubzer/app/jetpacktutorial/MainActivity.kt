d41e296745a74128f738bc10b0029ec0420aff63
Martin <martin@deubzer.net>
2022-06-28 21:54:41 +0300

    basic jetpack thingies

--- app/src/main/java/net/deubzer/app/jetpacktutorial/MainActivity.kt
+++ app/src/main/java/net/deubzer/app/jetpacktutorial/MainActivity.kt
@@ -1,29 +1,39 @@
 package net.deubzer.app.jetpacktutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.CornerSize
+
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import net.deubzer.app.jetpacktutorial.ui.theme.JetpacktutorialTheme
+import androidx.compose.foundation.border
+import androidx.compose.material3.Surface
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            //Text("Hello world!")
             JetpacktutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+                MessageCard(message = "positin am 23.6.2022 um 14:43", info = "48.23N 12.3E")
             }
         }
     }
@@ -34,10 +44,55 @@ fun Greeting(name: String) {
     Text(text = "Hello $name!")
 }
 
+@Composable
+fun MessageCard(message: String, info: String) {
+    Row(
+        Modifier
+            .background(Color.Gray)
+            .clip(RoundedCornerShape(15))
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.vitoshakite),
+            contentDescription = "image for test",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+                .border(1.5.dp, MaterialTheme.colorScheme.secondary, CircleShape)
+        )
+        Spacer(modifier = Modifier.width(8.dp))
+        Column {
+
+            Text(text = message)
+            Spacer(modifier = Modifier.height(4.dp))
+            Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 1.dp) {
+                Text(
+                    text = info,
+                    modifier = Modifier.height(20.dp),
+                    color = MaterialTheme.colorScheme.secondary,
+                    style = MaterialTheme.typography.bodySmall
+                )
+            }
+        }
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpacktutorialTheme {
-        Greeting("Android")
+        MessageCard(message = "bob am 12.4.2022 um 13.33", info = " 12E 14N")
+    }
+}
+
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode"
+)
+@Composable
+fun PreviewMessageCard() {
+    JetpacktutorialTheme {
+        MessageCard(message = "Rabbitmq könnte hier was schreiben!", info = "48.1N 10.2E")
     }
 }
